1e67ec5dd41dcd24765b4db9fbe7ae2de03fbf43
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-15 00:48:06 +0100

    trying to fix error Error while running '$ python manage.py collectstatic --noinput'.

--- web_studio/settings.py
+++ web_studio/settings.py
@@ -126,7 +126,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-DISABLE_COLLECTSTATIC=1
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -143,3 +143,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
