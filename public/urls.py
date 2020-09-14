a5430a19820c7c0f3fbb9d6329104da4410c5603
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-14 22:44:40 +0100

    created blog page with sections: what's included, extras and benefits

--- public/urls.py
+++ public/urls.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
 from django.urls import path
-from public.views import index
+from public.views import index,blog,get_in_touch,website,online_store
 
 urlpatterns = [
 
-    path('',index,name='hello'),
+    path('',index,name='home'),
+    path('get-in-touch', get_in_touch, name='get_in_touch'),
+    path('blog', blog, name='blog'),
+    path('website', website, name='website'),
+    path('online-store', online_store, name='online_store'),
 
 ]
--- public/views.py
+++ public/views.py
@@ -10,3 +10,17 @@ def index(request):
     }
     return render(request, 'public/index.html', context)
 
+def blog(request):
+
+    return render(request, 'public/blog.html')
+def website(request):
+
+    return render(request, 'public/website.html')
+def online_store(request):
+
+    return render(request, 'public/online_store.html')
+def get_in_touch(request):
+
+    return render(request, 'public/get_in_touch.html')
+
+
