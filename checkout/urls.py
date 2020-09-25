7d6b57e15e02efaf2b1caed4ed451b6907c23343
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-25 21:16:43 +0100

    put path('wh/', webhook, name='webhook'), on the top because it was overwritten by  path('<str:type>/',checkout,name='checkout'), and it was going to the wrong function protected by csrf again, wrong line moved before

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,13 +3,13 @@ from checkout.views import checkout,checkout_success,cache_checkout_data
 from .webhooks import webhook
 
 urlpatterns = [
-    path('<str:type>/', checkout, name='checkout'),
+    path('wh/', webhook, name='webhook'),
     path('<str:type>/',checkout,name='checkout'),
     path('checkout_success/<order_number>',
          checkout_success, name='checkout_success'),
     path('cache_checkout_data/', cache_checkout_data,
          name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
+
 
 
 ]
