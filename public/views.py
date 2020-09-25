d2ca6864039f3d064cd88a064a19bb959ee2f366
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-25 13:27:34 +0100

    reformatted the code and added some comments

--- public/views.py
+++ public/views.py
@@ -1,20 +1,14 @@
-import datetime
-
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-
-from .models import Appointment
 from .forms import AppointmentForm
-
-
+from .models import Appointment
 
 
 def index(request):
-
     return render(request, 'public/index.html')
 
 
@@ -30,55 +24,41 @@ def online_store(request):
     return render(request, 'public/online_store.html')
 
 
-
 def testimonials(request):
     return render(request, 'public/testimonials.html')
 
+
 def portfolio(request):
-     return render(request, 'public/portfolio.html')
+    return render(request, 'public/portfolio.html')
+
 
 @login_required
 def profile(request):
     """
-
-
-    :param request:
-    :return:
     user can log in to his account to update or delete appointment
     """
 
     template = 'public/profile.html'
-    try:
-        consultation_detail = Appointment.objects.get(email=request.user.email)
-    except:
-        # in case there is no consultation in the db, we will redirect user to
-        # no consultation page
-        return render(request, 'public/no_consultation.html')
 
-    if consultation_detail:
-        context = {
-            'user': request.user.username,
-            'consultations': consultation_detail
+    consultation_detail = Appointment.objects.get(email=request.user.email)
 
-        }
-    else:
-        template = 'public/index.html'
-        context = {
-            'user': request.user.username,
-            'consultations': consultation_detail
+    context = {
+        'user': request.user.username,
+        'consultations': consultation_detail
 
-        }
+    }
 
     return render(request, template, context)
+
 
 """
 POTENTIAL CUSTOMER CAN BOOK FREE CONSULTATION
 """
 
-def consultations(request):
 
+def consultations(request):
     template = 'public/consultation.html'
-    context ={}
+    context = {}
 
     if request.method == 'POST':
 
@@ -88,28 +68,38 @@ def consultations(request):
 
             return False
 
+        def user_email_present(email):
+            if User.objects.filter(email=email).exists():
+                return True
 
+            return False
 
         form_data = {
             'name': request.POST['name'],
             'email': request.POST['email'],
             'phone_num': request.POST['phone_num'],
             'password': 'user_password',
-            'site_type':  request.POST['site_type'],
+            'site_type': request.POST['site_type'],
             'time_slot': request.POST['time_slot'],
             'project': request.POST['project'],
-            'done':  'done' in request.POST,
+            'done': 'done' in request.POST,
         }
 
         consultation_form = AppointmentForm(form_data)
         context = {
-            'form':consultation_form
+            'form': consultation_form
         }
         if user_name_present(request.POST['name']):
             messages.error(request, 'Please use different user name !')
 
             return render(request, template, context)
 
+        if user_email_present(request.POST['email']):
+            messages.error(request,
+                           'We have user with this email registered already! You can login to see your appointment '
+                           ' or use different email to create new appointment.')
+
+            return render(request, template, context)
 
         if consultation_form.is_valid():
             try:
@@ -120,11 +110,12 @@ def consultations(request):
                 CHANGE HIS CONSULTATION
                 """
                 consultation_form.save()
-                new_user = User.objects.create_user(request.POST['name'] ,
+                new_user = User.objects.create_user(request.POST['name'],
                                                     request.POST['email'],
                                                     request.POST['password'])
                 new_user.save()
-                messages.success(request, 'Your account was created successfully \ Please login with your '
+
+                messages.success(request, 'Your account was created successfully. Please login with your '
                                           'credentials.')
                 return redirect(reverse('profile'))
             except:
@@ -132,11 +123,10 @@ def consultations(request):
                                                               Please double check your information.')
         context['consultation_form'] = consultation_form
 
-    return render(request, template,context)
+    return render(request, template, context)
 
 
 def edit_consultation(request, item_id):
-
     item = get_object_or_404(Appointment, id=item_id)
 
     if request.method == 'POST':
@@ -151,14 +141,17 @@ def edit_consultation(request, item_id):
     form = AppointmentForm(instance=item)
     context = {
         'form': form,
-        'item_id':item_id,
-        'email':item.email
+        'item_id': item_id,
+        'email': item.email
     }
     return render(request, 'public/edit_consultation.html', context)
+
 
 """
 WHEN USER IS DELETING APPOINTMENT, WE WILL DELETE HIS ACCOUNT AS WELL
 """
+
+
 def delete_consultation(request, item_id):
     item = get_object_or_404(Appointment, id=item_id)
     item.delete()
@@ -171,8 +164,3 @@ def delete_consultation(request, item_id):
         messages.error(request, "User does not exist")
 
     return redirect('home')
-
-
-
-
-
