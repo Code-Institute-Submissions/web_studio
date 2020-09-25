51d6cda4a110b89c02da3d060a91d17cd5330958
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-25 08:56:32 +0100

    removed csrf token from checkout form, to see if 403 error will be fixed

--- public/urls.py
+++ public/urls.py
@@ -7,7 +7,6 @@ from public.views import index,blog,get_in_touch,website,online_store,testimonia
 urlpatterns = [
 
     path('',index,name='home'),
-    path('get-in-touch', get_in_touch, name='get_in_touch'),
     path('blog', blog, name='blog'),
     path('website', website, name='website'),
     path('online-store', online_store, name='online_store'),
@@ -15,9 +14,7 @@ urlpatterns = [
     path('portfolio', portfolio, name='portfolio'),
     path('consultations', consultations, name='consultations'),
     path('edit/consultation/<item_id>', edit_item, name='edit_item'),
-
     path('delete/consultation/<item_id>', delete_item, name='delete_item'),
-    path('edit_consultation', edit_consultation, name='edit_consultation'),
     path('booking_success', booking_success, name='booking_success'),
     path('profile', profile, name='profile'),
 
