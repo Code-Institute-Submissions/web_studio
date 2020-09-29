4e46b7355a399c6b559ce36c5a741e33b89c38f8
Marcel Kolarcik <60097861+marcelkolarcik@users.noreply.github.com>
2020-09-29 19:38:34 +0100

    fixed link to blog on index.html, added favicon

--- checkout/test_checkout_view.py
+++ checkout/test_checkout_view.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 
 
 class TestCheckoutView(TestCase):
-    def test_index_page(self):
+    def test_checkout_page(self):
         response = self.client.get('/checkout/blog/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'checkout/checkout.html')
--- checkout/test_order_model.py
+++ checkout/test_order_model.py
@@ -3,10 +3,7 @@ from django.test import TestCase
 from .models import Order
 
 
-
 class TestModels(TestCase):
-
-
 
     def test_customer_name_repr_method_returns_name(self):
         order = Order.objects.create(name='Customer Name')
